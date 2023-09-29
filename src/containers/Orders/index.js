2d39d713ed6f43865f743863a8ca1bd5b03c1597
SLUZ56 <sd54diasinacio@gmail.com>
2023-09-29 17:10:11 -0300

    new files

---
+++ src/containers/Orders/index.js
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
+
+import BurgerBag from '../../assets/burger.png'
+import Trash from '../../assets/trash.svg'
+import axios from 'axios'
+// const axios = require('axios/dist/browser/axios.cjs'); // browser
+
+import { Container, H1, Image, ContainerItems, Button, Order  } from './styles'
+
+export default function Orders () {
+
+  const [orders, setOrders] = useState([])  // array de pedidos
+  const history = useHistory()  
+ 
+
+ // REACT HOOK => useEffect
+ // A minha aplicação inicia o useEffect é chamado
+ useEffect(() => {
+    async function fetchOrders() {
+      const { data: newOrders } = await axios.get("http://localhost:3002/orders")
+
+      setOrders(newOrders)
+
+  }
+
+  fetchOrders()
+  
+ }, []) 
+
+
+
+
+ async function deleteOrder(orderId){
+     await axios.delete(`http://localhost:3002/orders/${orderId}`)
+     const newOrders = orders.filter(order => order.id !== orderId)
+
+     setOrders(newOrders)
+  }
+  
+
+  function goBackPage(){
+     
+    history.push("/")
+
+  }
+   
+   return(
+
+        <Container>
+
+          <Image  alt="logo-imagem"  src={BurgerBag}/>
+
+          <ContainerItems>
+
+          <H1>Pedidos</H1>
+
+          <>
+           <>
+              <ul>
+                  { orders.map( order => (
+                      <Order key={order.id}>
+                        <p>{order.order}</p> <p>{order.name}</p> <p>{order.price}</p> <p>{order.status}</p> 
+                        <button onClick={() => deleteOrder(order.id)}>
+                          <img src={Trash} alt="lata-de-lixo"/>
+                        </button> 
+                      </Order>
+
+                    ))
+                  }
+              </ul>
+              </> 
+          </>
+
+          <Button onClick={goBackPage}>Voltar</Button>
+
+          </ContainerItems>
+           
+        </Container>
+   )
+
+}
+
+ 
---
+++ src/containers/Orders/styles.js
@@ -0,0 +1,128 @@
+import styled from 'styled-components'
+
+import Background from '../../assets/bg.png'
+
+export const Container = styled.div `
+  background: url("${Background}");
+  
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 40px;
+  height: 100%;
+  min-height: 100vh;
+  
+
+`
+
+export const Image = styled.img `
+    width: 246px;
+    height: 354px;
+    margin: 11px 36px 0px 24px;
+ 
+  
+
+`
+
+export const ContainerItems = styled.div `
+    padding: 50px 36px;
+    display: flex;
+    flex-direction: column; 
+
+
+`
+export const H1 = styled.h1 `
+    color: white;
+    font-size: 28px;
+    font-weight: 700;
+    word-wrap: break-word;
+    margin-top: -30px;
+    margin-bottom: 20px;
+    line-height: 40px;
+    text-align: center;
+
+    
+
+`
+
+
+export const Button= styled.button `
+    width: 342px;
+    height: 68px;
+    background-color: rgba(255, 255, 255, 0.14);
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    margin: 0 auto;
+    margin-top: 76px;
+    
+
+    color: white;
+    font-size: 17px;
+    font-weight: 900;
+    word-wrap: break-word;
+    padding: 5px; 
+    text-align: center;
+
+    display: flex;
+    justify-content:center;
+    align-items: center;
+    gap: 20px;
+
+    &:hover {
+      opacity: 0.8;
+    }
+
+    &:active {
+      opacity: 0.5;
+    }
+
+`
+
+
+export const Order = styled.li `
+    display: flex;
+    flex-direction: column;
+    margin-top: 20px;
+    padding: 10px;
+
+    background: rgba(255, 255, 255, 0.25);
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 14px;
+
+    width: 342px;
+    height: 110px;
+
+    border: none;
+    outline: none;
+
+    p {
+     
+        display: flex;
+        flex-direction: column;
+        
+        color: #757575;
+        font-size: 16px;
+        font-weight: 300;
+        word-wrap: break-word;
+        padding: 5px; 
+        text-align: left;
+    
+     }
+ 
+    
+    button {
+      /* position: relative; */
+      margin-top: -40px;
+      display: flex;
+      flex-direction: row;
+      justify-content: end; 
+      align-items: center;
+      background: none;
+      border: none;
+      cursor: pointer;
+      /* color: rgba(255, 255, 255, 0.14); */
+    }
+
+   
+`
